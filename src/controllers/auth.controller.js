855f8c23278574e39c85ac3a2369613aace17547
D33PAKSHARMA <deepak02580@gmail.com>
2023-12-03 20:57:20 +0530

    add validation

--- src/controllers/auth.controller.js
+++ src/controllers/auth.controller.js
@@ -1,10 +1,21 @@
+import Joi from "joi";
 import { User } from "../models/user.model.js";
 import { ApiError } from "../utils/apiErrors.js";
 import { AsyncErrors } from "../utils/asyncErrorHandler.js";
 
-const registerUser = AsyncErrors(async (req, res) => {
-  //   const { email, name, password } = req.body;
-  console.log("req.body==========>", req.body);
+const registerUser = AsyncErrors(async (req, res, next) => {
+  // validations
+  const registerSchema = Joi.object({
+    name: Joi.string().min(3).max(30).required(),
+    email: Joi.string().email().required(),
+    password: Joi.string()
+      .pattern(new RegExp("^[a-zA-Z0-9]{6,30}$"))
+      .required(),
+  });
+  const { error } = registerSchema.validate(req.body);
+  if (error) {
+    return next(new ApiError(501, error.details[0].message));
+  }
 
   //   const user = await User.findOne({ email });
 
